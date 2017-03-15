807755b4bd9d3f9f24baf3c5e54c1d6adda0bd4c
thisLight <l1589002388@gmail.com>
2017-03-15 11:12:17 +0800

    Add example for blueprint

---
+++ example/with-blueprint.py
@@ -0,0 +1,37 @@
+"""
+Wood author: thislight
+
+Copyright 2016 thsilight
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+Under License Apache v2, more infomation, see file 'LICENSE' in project root directory.
+"""
+
+from wood.support import Blueprint
+from wood import Wood
+
+b = Blueprint()
+
+Index = b.empty(r"/","IndexHandler")
+
+@Index.get
+def index(self):
+    self.write("Hello, world")
+
+w = Wood(__name__,debug=True)
+
+b.to(w)
+
+w.start(6000)
+
