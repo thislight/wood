7b57cc7dd326bdd1615ea8bab2d173dd1b935e10
thisLight <l1589002388@gmail.com>
2017-03-16 08:31:47 +0800

    Fix some bugs and add a method for clean environment.

--- wood/support/event.py
+++ wood/support/event.py
@@ -7,23 +7,26 @@ class EventManager(object):
     @classmethod
     def callback(cls,event,callback):
         if event not in cls.callbacks:
-            cls.callbacks.extend({
-                event:[]
-            })
+            cls.callbacks[event] = []
 
         cls.callbacks[event].append(callback)
 
     @classmethod
     def call(cls,event,*args,**kargs):
-        cls.event_queue.append(tuple(event,args,kargs))
+        cls.event_queue.append((event,args,kargs))
 
     @classmethod
     def finish_queue(cls):
         while len(cls.event_queue) > 0:
             name,args,kargs = cls.event_queue.pop()
             if name in cls.callbacks:
-                for callback in cls.callbacks:
+                for callback in cls.callbacks[name]:
                     callback(*args,**kargs)
+
+    @classmethod
+    def clean_env(cls):
+        cls.callbacks = {}
+        cls.event_queue = []
 
 
 def event_callback(event):
@@ -32,6 +35,6 @@ def event_callback(event):
         EventManager.callback(event,func)
         return func
 
-    return func
+    return set_callback
 
 
